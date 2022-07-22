2a1490d7c29960d194f4918124073b5cc2b82fd1
randronaco <randronaco1027@gmail.com>
2022-07-22 17:03:16 -0400

    added save_book to searchbooksjs

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -4,23 +4,22 @@ import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'reac
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import {useMutation} from '@apollo/client'
+import {GET_ME} from '../utils/queries'
+import { ADD_USER, SAVE_BOOK } from '../utils/mutations';
 
 const SearchBooks = () => {
-  // create state for holding returned google api data
+ 
   const [searchedBooks, setSearchedBooks] = useState([]);
-  // create state for holding our search field data
+  
   const [searchInput, setSearchInput] = useState('');
 
-  // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
-  // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
-  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
 
-  // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -52,6 +51,7 @@ const SearchBooks = () => {
     }
   };
 
+  const saveBook = useMutation(SAVE_BOOK)
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
@@ -65,7 +65,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({variables: {input: bookToSave}});
 
       if (!response.ok) {
         throw new Error('something went wrong!');
