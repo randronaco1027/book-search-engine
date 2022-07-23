1d425de514b8f06759b7f31b183b9cc4d46b0341
randronaco <randronaco1027@gmail.com>
2022-07-23 16:26:53 -0400

    fixed login

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -28,15 +28,12 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await login({variables: {...userFormData}});
+      const {data} = await login({variables: {...userFormData}});
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      Auth.login(data.login.token);
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      
+      
     } catch (err) {
       console.error(err);
       setShowAlert(true);
